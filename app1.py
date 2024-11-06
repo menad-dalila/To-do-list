9a4b9b6022aaa912c746cb07370c6f5dc68090fc
blubery-glitch <141056439+blubery-glitch@users.noreply.github.com>
2024-11-06 09:27:39 +0100

    added html

--- app1.py
+++ app1.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import views 
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
